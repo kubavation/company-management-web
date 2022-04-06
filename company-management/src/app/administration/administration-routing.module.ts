dc3a394687e7805b0c23d9a024fc284fff00a4df
kubavation <kubavation@wp.pl>
2022-04-06 17:15:10 +0200

    added angular material

--- company-management/src/app/administration/administration-routing.module.ts
+++ company-management/src/app/administration/administration-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Route, RouterModule, Routes} from "@angular/router";
+import {AdministrationComponent} from "./administration/administration.component";
 
 const routes: Route[] = [
-
+  {path: '', component: AdministrationComponent}
 ]
 
 @NgModule({
--- company-management/src/app/administration/administration.module.ts
+++ company-management/src/app/administration/administration.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdministrationRoutingModule } from './administration-routing.module';
+import { AdministrationComponent } from './administration/administration.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [AdministrationComponent],
   imports: [
     CommonModule,
     AdministrationRoutingModule
