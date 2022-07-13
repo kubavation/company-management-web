93ee0329c0b1535b0de4ed455a7c38fa19b2b270
kubavation <kubavation@wp.pl>
2022-07-13 17:03:37 +0200

    connection with backend -> menu options

--- company-management/src/app/layout/service/layout.service.ts
+++ company-management/src/app/layout/service/layout.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable, of} from "rxjs";
 import {MenuOption} from "../model/menu-option";
 import {environment as Env} from "../../../environments/environment";
+import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,9 @@ export class LayoutService {
   constructor(private http: HttpClient) { }
 
   public getMenuOptions(): Observable<MenuOption[]> {
-   // return this.http.get<MenuOption[]>(`${Env}/configuration/menu-options`);
-    return of([
-      {name: 'ADMINISTRATION', url: '/administration'}
-    ])
+    return this.http.get<MenuOption[]>(`${Env.serverUrl}/configuration/group/MENU_OPTION`)
+      .pipe(
+        tap((_) => console.log(_))
+      );
   }
 }
--- company-management/src/environments/environment.ts
+++ company-management/src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:8080'
+  serverUrl: 'http://localhost:8081'
 };
 
