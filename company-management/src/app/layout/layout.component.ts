199024ac381303ccaa1747ba05886a1971c6bceb
kubavation <kubavation@wp.pl>
2022-08-09 19:29:54 +0200

    styling layout

--- company-management/src/app/layout/layout.component.ts
+++ company-management/src/app/layout/layout.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, ViewChild} from '@angular/core';
 import {LayoutService} from "./service/layout.service";
+import {MatSidenav} from "@angular/material/sidenav";
 import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  styleUrls: ['./layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutComponent {
 
-  menuOptions$ = this.layoutService.getMenuOptions();
+  @ViewChild('sidenav') sidenav: MatSidenav;
+
+  menuOptions$ = this.layoutService.getMenuOptions().pipe(
+    tap(() => this.openSidenav())
+  );
 
   constructor(private layoutService: LayoutService) { }
 
 
+  openSidenav(): void {
+    this.sidenav.open();
+  }
 
 
 }
