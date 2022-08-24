07abba6ee342bd20ad6cf1f5d7686529e627cb93
jakub.durys <duryskuba@wp.pl>
2022-08-24 18:50:34 +0200

    leave request model

--- company-management/src/app/leave-requests/leave-request-list/leave-request-list.component.ts
+++ company-management/src/app/leave-requests/leave-request-list/leave-request-list.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-leave-request-list',
   templateUrl: './leave-request-list.component.html',
-  styleUrls: ['./leave-request-list.component.scss']
+  styleUrls: ['./leave-request-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LeaveRequestListComponent {
+
+  @Input() leaveRequests: any[];
 
   constructor() { }
 
