ebbbf05cf0fcd6bb8bca51cb1a95804cfe3b97d9
jakub.durys <duryskuba@wp.pl>
2022-08-25 21:18:52 +0200

    default leave request type

--- company-management/src/app/leave-requests/leave-requests.component.ts
+++ company-management/src/app/leave-requests/leave-requests.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {LeaveRequestService} from "./service/leave-request.service";
 import {EmployeeBsService} from "../shared/employee/service/employee-bs.service";
 import {filter, switchMap, tap} from "rxjs/operators";
+import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {LeaveRequestType} from "./model/leave-request-type";
 
 @Component({
   selector: 'app-leave-requests',
@@ -10,16 +12,23 @@ import {filter, switchMap, tap} from "rxjs/operators";
 })
 export class LeaveRequestsComponent {
 
-  defa
+  requestTypeForm = this.fb.group({
+    requestType: [null, Validators.required]
+  })
 
-  leaveRequestTypes$ = this.leaveRequestService.findLeaveRequestTypes();
+  leaveRequestTypes$ = this.leaveRequestService.findLeaveRequestTypes().pipe(
+     tap((types) => {
+       this.requestTypeForm.patchValue({requestType:  types.find(t => t.value  === LeaveRequestType.ANNUAL)})
+     })
+  );
 
   leaveRequests$ = this.employeesBsService.employee$.pipe(
     switchMap((employee) => this.leaveRequestService.findByEmployeeId(employee?.id)));
 
 
   constructor(private leaveRequestService: LeaveRequestService,
-              private employeesBsService: EmployeeBsService) { }
+              private employeesBsService: EmployeeBsService,
+              private fb: FormBuilder) { }
 
 
 }
