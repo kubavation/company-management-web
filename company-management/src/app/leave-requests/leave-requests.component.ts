c3312469e4c8edef96307417a7db4950dcea6d38
jakub.durys <duryskuba@wp.pl>
2022-09-19 20:00:26 +0200

    default value in leave request type

--- company-management/src/app/leave-requests/leave-requests.component.ts
+++ company-management/src/app/leave-requests/leave-requests.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {LeaveRequestService} from "./service/leave-request.service";
 import {EmployeeBsService} from "../shared/employee/service/employee-bs.service";
-import {filter, switchMap, tap} from "rxjs/operators";
+import {filter, startWith, switchMap, tap} from "rxjs/operators";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
 import {LeaveRequestType} from "./model/leave-request-type";
+import {KeyValue} from "../shared/model/key-value";
+import {combineLatest} from "rxjs";
 
 @Component({
   selector: 'app-leave-requests',
@@ -12,23 +14,25 @@ import {LeaveRequestType} from "./model/leave-request-type";
 })
 export class LeaveRequestsComponent {
 
-  requestTypeForm = this.fb.group({
-    requestType: [null, Validators.required]
-  })
+  leaveRequestTypeControl = new FormControl(LeaveRequestType.ANNUAL);
 
-  leaveRequestTypes$ = this.leaveRequestService.findLeaveRequestTypes().pipe(
-     tap((types) => {
-       this.requestTypeForm.patchValue({requestType:  types.find(t => t.value  === LeaveRequestType.ANNUAL)})
-     })
+  leaveRequestTypeControlValue$ = this.leaveRequestTypeControl.valueChanges
+    .pipe(
+      startWith(LeaveRequestType.ANNUAL)
   );
 
-  leaveRequests$ = this.employeesBsService.employee$.pipe(
-    switchMap((employee) => this.leaveRequestService.findByEmployeeId(employee?.id)));
+  leaveRequestTypes$ = this.leaveRequestService.findLeaveRequestTypes();
+
+  leaveRequests$ = combineLatest([this.employeesBsService.employee$, this.leaveRequestTypeControlValue$])
+    .pipe(
+      switchMap(([employee, requestType]) => this.leaveRequestService.findByEmployeeId(employee?.id))
+    );
 
 
   constructor(private leaveRequestService: LeaveRequestService,
-              private employeesBsService: EmployeeBsService,
-              private fb: FormBuilder) { }
+              private employeesBsService: EmployeeBsService) {
 
+
+  }
 
 }
