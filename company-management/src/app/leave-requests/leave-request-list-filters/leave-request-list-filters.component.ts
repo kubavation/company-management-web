6615fe2c241270cdb7573b34d3694b10b0ab4835
jakub.durys <duryskuba@wp.pl>
2022-09-20 20:11:27 +0200

    toggle for filters + styling

--- company-management/src/app/leave-requests/leave-request-list-filters/leave-request-list-filters.component.ts
+++ company-management/src/app/leave-requests/leave-request-list-filters/leave-request-list-filters.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-leave-request-list-filters',
@@ -7,6 +7,8 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LeaveRequestListFiltersComponent {
+
+  @Input() enabled = false;
 
   constructor() { }
 
--- company-management/src/app/leave-requests/leave-requests.component.ts
+++ company-management/src/app/leave-requests/leave-requests.component.ts
@@ -41,4 +41,5 @@ export class LeaveRequestsComponent {
     console.log(checked)
   }
 
+
 }
