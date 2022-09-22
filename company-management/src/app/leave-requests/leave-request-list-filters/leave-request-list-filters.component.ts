f17ddb1e692dd5433f473c894e638beab4fe3e26
jakub.durys <duryskuba@wp.pl>
2022-09-22 20:23:36 +0200

    init of leave request chips

--- company-management/src/app/leave-requests/leave-request-list-filters/leave-request-list-filters.component.ts
+++ company-management/src/app/leave-requests/leave-request-list-filters/leave-request-list-filters.component.ts
@@ -1,5 +1,8 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
+import {KeyValue} from "../../shared/model/key-value";
+import {MatChipInputEvent} from "@angular/material/chips";
+import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
 
 @Component({
   selector: 'app-leave-request-list-filters',
@@ -10,10 +13,14 @@ import {FormBuilder} from "@angular/forms";
 export class LeaveRequestListFiltersComponent {
 
   @Input() enabled = false;
+  @Input() requestTypes: KeyValue<string>[];
+
+  private filteredRequestTypes: string[] = [];
 
   form = this.fb.group({
         dateFrom: [null],
-        dateTo: [null]
+        dateTo: [null],
+        requestType: [null]
     }
   )
 
@@ -23,6 +30,35 @@ export class LeaveRequestListFiltersComponent {
 
   search(): void {
     console.log(this.form.value)
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    if ((value || '').trim()) {
+      this.filteredRequestTypes.push(value.trim());
+    }
+
+    if (input) {
+      input.value = '';
+    }
+
+    this.form.get('requestType').setValue(null);
+  }
+
+  remove(type: string): void {
+    const index = this.filteredRequestTypes.indexOf(type);
+
+    if (index >= 0) {
+      this.filteredRequestTypes.splice(index, 1);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    this.filteredRequestTypes.push(event.option.viewValue);
+    //this.fruitInput.nativeElement.value = ''; //todo
+    this.form.get('requestType').setValue(null);
   }
 
 
