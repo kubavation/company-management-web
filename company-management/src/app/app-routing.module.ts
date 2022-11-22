4bc8a08fb8981b1323bc32dc1781d6102f50a021
kubavation <37585627+kubavation@users.noreply.github.com>
2022-11-22 16:28:12 +0100

    authGuard connect

--- company-management/src/app/app-routing.module.ts
+++ company-management/src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
+import {AuthGuard} from "./keycloak/guards/auth.guard";
 
 const routes: Routes = [
   {
     path: 'administration',
-    loadChildren: () => import('./administration/administration.module').then(m => m.AdministrationModule)
+    loadChildren: () => import('./administration/administration.module').then(m => m.AdministrationModule),
+    canActivate: [AuthGuard]
   },
   {
     path: 'leave-requests',
-    loadChildren: () => import('./leave-requests/leave-requests.module').then(m => m.LeaveRequestsModule)
+    loadChildren: () => import('./leave-requests/leave-requests.module').then(m => m.LeaveRequestsModule),
+    canActivate: [AuthGuard]
   },
   {
-    path: '**', redirectTo: ''
+    path: '**', redirectTo: '',
+    canActivate: [AuthGuard]
   }
 ];
 
